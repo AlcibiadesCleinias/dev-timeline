2f1495aac9430a6f866e6e75f322a3d7d14f09e5
AlcibiadesCleinias <ivan.cheprasov@phystech.edu>
2024-07-14 00:12:36 +0200

    Updates

---
+++ src/components/Constants/colors.js
@@ -0,0 +1,20 @@
+// Kinda deprecated - use theme colors.
+
+import { THEME } from "./themes";
+
+export const educationColor = {
+  primary: THEME.palette.error.main,
+  dimmed: "rgba(211,47,47,0.64)",
+};
+export const projectColor = {
+  primary: THEME.palette.info.main,
+  dimmed: "rgb(33, 150, 243, 0.5)",
+};
+export const prizeColor = {
+  primary: "#FFD700",
+  dimmed: "rgb(255, 215, 0, 0.5)",
+};
+export const workColor = {
+  primary: THEME.palette.success.main,
+  dimmed: "rgb(67, 160, 71, 0.5)",
+};
