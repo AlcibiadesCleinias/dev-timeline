0d3581731887adb576f428c5a2860ccffa8fa3d7
AlcibiadesCleinias <ivan.cheprasov@phystech.edu>
2024-07-13 11:51:57 +0200

    add description formatter

--- src/components/VerticalTimelineElements/utils.js
+++ src/components/VerticalTimelineElements/utils.js
@@ -1,3 +1,8 @@
 export function formatDateToString(date) {
     return `${date.toLocaleString('default', { month: 'short' })} ${date.getFullYear()}`
 }
+
+export function prettifyDescription(description) {
+    const s = description.split("\\n")[0]
+    return `${s} ...`
+}
