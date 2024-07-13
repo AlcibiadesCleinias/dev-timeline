9180fc83b154df86c7f357dac807707ae492897e
AlcibiadesCleinias <AlcibiadesCleinias@users.noreply.github.com>
2024-07-13 22:15:00 +0000

    deploy: 12acfe00977718daafe73a7e7704f4113ce35ab5

--- src/components/VerticalTimelineElements/utils.js
+++
@@ -1,8 +0,0 @@
-export function formatDateToString(date) {
-  return `${date.toLocaleString("default", { month: "short" })} ${date.getFullYear()}`;
-}
-
-export function prettifyDescription(description) {
-  const s = description.split("\n")[0];
-  return `${s} ...`;
-}
